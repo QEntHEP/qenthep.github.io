ba0ac2c035d040720537bc137f9c7baec3f746ce
Hugo Cachitas <hcachitas@gmail.com>
2023-07-07 19:40:52 +0100

    Update project configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				display: ['Inter', 'system-ui', 'sans-serif'],
+				body: ['Inter', 'system-ui', 'sans-serif'],
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
-}
+};
