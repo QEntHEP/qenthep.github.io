9bf28280cc4df003817084894d364df3f17315c0
Hugo Cachitas <hcachitas@gmail.com>
2023-07-08 04:14:06 +0100

    Add markdown plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import remarkEmoji from 'remark-emoji';
+import rehypeKatex from 'rehype-katex';
+import { remarkReadingTime } from './remark-plugins/remark-reading-time.mjs';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://qenthep.github.io',
 	integrations: [tailwind(), mdx()],
 	experimental: {
 		assets: true,
+	},
+	markdown: {
+		remarkPlugins: [remarkReadingTime, remarkMath, remarkEmoji],
+		rehypePlugins: [rehypeKatex],
 	},
 });
