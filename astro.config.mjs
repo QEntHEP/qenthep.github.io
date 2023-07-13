03f5341aaffc725580e608afe284ef3627b6a888
Hugo Cachitas <hcachitas@gmail.com>
2023-07-13 22:36:13 +0100

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,22 @@ import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import remarkEmoji from 'remark-emoji';
 import rehypeKatex from 'rehype-katex';
+import icon from 'astro-icon';
 import { remarkReadingTime } from './remark-plugins/remark-reading-time.mjs';
 
 export default defineConfig({
 	site: 'https://qenthep.github.io',
-	integrations: [tailwind(), mdx()],
+	integrations: [
+		tailwind(),
+		mdx(),
+		icon({
+			include: {
+				academicons: ['*'],
+				heroicons: ['*'],
+				logos: ['*'],
+			},
+		}),
+	],
 	experimental: {
 		assets: true,
 	},
