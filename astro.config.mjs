519e5890403d7d9cd7da7957196c679e32ef1fa3
Hugo Cachitas <hcachitas@gmail.com>
2023-07-15 13:19:24 +0100

    Add alpinejs integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import alpinejs from '@astrojs/alpinejs';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
@@ -7,9 +8,11 @@ import rehypeKatex from 'rehype-katex';
 import icon from 'astro-icon';
 import { remarkReadingTime } from './remark-plugins/remark-reading-time.mjs';
 
+// https://astro.build/config
 export default defineConfig({
 	site: 'https://qenthep.github.io',
 	integrations: [
+		alpinejs(),
 		tailwind(),
 		mdx(),
 		icon({
